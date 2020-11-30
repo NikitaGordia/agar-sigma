c01a41860435f962cc8f5457168a314c60e37b81
@Pavlo.Bohdan <pavlo.bohdan@nordstrom.com>
2020-11-30 20:48:49 +0200

    Integrate ChunkController

--- src/main/kotlin/com/sigma/agar/Game.kt
+++ src/main/kotlin/com/sigma/agar/Game.kt
@@ -1,25 +1,34 @@
 package com.sigma.agar
 
+import com.sigma.agar.physics.ChunkController
 import com.sigma.agar.physics.World
-import com.sigma.agar.physics.entity.Player
-import com.sigma.agar.physics.entity.Snack
-import com.sigma.agar.physics.entity.Thing
+import com.sigma.agar.physics.things.Player
+import com.sigma.agar.physics.things.Snack
+import com.sigma.agar.physics.things.Thing
 import com.sigma.agar.utils.GameLoop
 import com.sigma.agar.utils.GameLoop.GameLoopListener
 import com.sigma.agar.utils.Utils
 import com.sigma.agar.utils.Tuple
 import com.sigma.agar.utils.protocol.Protocol
-import org.web.httpserver.Session
+import org.webutil.httpserver.Session
 import java.util.*
 
 class Game : GameLoop(), GameLoopListener {
 
     companion object {
-        const val FPS = 60
-        const val WORLD_WIDTH = 2500
-        const val WORLD_HEIGHT = 2500
+        const val FPS = 30
+        const val WORLD_WIDTH = 1500
+        const val WORLD_HEIGHT = 1500
         const val TOTAL_COLORS = 10
-        const val INIT_FOOD_COUNT = 2000
+        const val INIT_FOOD_COUNT = 300
+        const val POV_WIDTH = 500
+        const val POV_HEIGHT = 500
+        val chunks = ChunkController(
+                pov_width = POV_WIDTH,
+                pov_height = POV_HEIGHT,
+                world_height = WORLD_HEIGHT,
+                world_width = WORLD_WIDTH
+        )
     }
 
     init {
@@ -28,13 +37,15 @@ class Game : GameLoop(), GameLoopListener {
         create()
     }
 
-    private val cache1 = Tuple()
+    var protocol_data: Protocol? = null
+
+    private val sessions = mutableSetOf<Session>()
+
     private val cache2 = Tuple()
     private var time_accumulator = 0f
     private var world: World? = null
     private val post_requests: MutableList<Runnable> = ArrayList()
     private val game_map: MutableMap<Long, Player> = HashMap()
-    private var protocol_data: Protocol? = null
 
     private fun create() {
         val random = Utils.random
@@ -42,11 +53,11 @@ class Game : GameLoop(), GameLoopListener {
         val half_height = WORLD_HEIGHT / 2
         val width = WORLD_WIDTH
         val height = WORLD_HEIGHT
-        world = object : World(WORLD_WIDTH.toFloat(), WORLD_HEIGHT.toFloat()) {
+        world = object : World(this@Game, WORLD_WIDTH.toFloat(), WORLD_HEIGHT.toFloat()) {
             override fun remove(thing: Thing) {
                 super.remove(thing)
                 if (thing.type == Thing.SNACK) {
-                    val _thing: Thing = Snack(0)
+                    val _thing: Thing = Snack(random.nextInt(TOTAL_COLORS))
                     _thing.position[random.nextInt(width) - half_width.toFloat()] = random.nextInt(height) - half_height.toFloat()
                     world!!.add(_thing)
                 }
@@ -54,9 +65,9 @@ class Game : GameLoop(), GameLoopListener {
         }
         var thing: Thing
         for (i in 0 until INIT_FOOD_COUNT) {
-            thing = Snack(0)
+            thing = Snack(random.nextInt(TOTAL_COLORS))
             thing.position[random.nextInt(width) - half_width.toFloat()] = random.nextInt(height) - half_height.toFloat()
-            world?.add(thing)
+            world!!.add(thing)
         }
     }
 
@@ -68,35 +79,38 @@ class Game : GameLoop(), GameLoopListener {
             time_accumulator -= World.TIME_UNIT
         }
         synchronized(post_requests) {
-            post_requests.forEach(Runnable::run)
+            post_requests.forEach { it.run() }
             post_requests.clear()
         }
     }
 
     override fun render() {
-        val things: MutableList<Thing> = ArrayList()
-        var size: Float
         for (player in game_map.values) {
+            player.update()
+
+            val things = chunks.get_neighbours(player)
+
+            protocol_data?.dispatch(
+                    player.bubble.position,
+                    POV_WIDTH.toFloat(),
+                    POV_HEIGHT.toFloat(),
+                    things,
+                    round_end_time_ms - System.currentTimeMillis(),
+                    player.session!!
+            )
+
             player.run {
-                update()
-                get_center(cache1)
+                follow(cache2.set(screen_point.x, screen_point.y).plus(bubble.position))
             }
-            box_search.set(player)
-            size = 200 + box_search.area * 0.0001f
-            box_search.run {
-                min.minus(size, size)
-                max.plus(size, size)
-            }
-            world!!.box_search.query(aabb, things)
-            if (things.isNotEmpty()) {
-                box_search.run {
-                    protocol_data!!.dispatch(cache1, width, height, things, player.session)
-                }
-                things.clear()
-            }
-            player.run {
-                follow(cache2.set(screen_point.x, screen_point.y).plus(cache1))
-            }
+        }
+    }
+
+    override fun reset_round() {
+        sessions.forEach {
+            unjoin(it)
+        }
+        sessions.forEach {
+            join(it)
         }
     }
 
@@ -104,9 +118,9 @@ class Game : GameLoop(), GameLoopListener {
         synchronized(post_requests) { post_requests.add(runnable) }
     }
 
-    fun join_player(session: Session) {
-        val half_width = world!!.max.x.toInt()
-        val half_height = world!!.max.y.toInt()
+    fun join(session: Session) {
+        val half_width = 2500
+        val half_height = 2500
         val width = half_width * 2
         val height = half_height * 2
         val random = Utils.random
@@ -114,8 +128,8 @@ class Game : GameLoop(), GameLoopListener {
         val y = random.nextInt(height) - half_height.toFloat()
         val color = random.nextInt(TOTAL_COLORS)
         add_post_runnable {
-            val player = Player(x, y, color, world!!)
-            player.session = session
+            sessions += session
+            val player = Player(session, x, y, color, world!!)
             session.userData = player
             game_map[session.id] = player
         }
@@ -124,31 +138,19 @@ class Game : GameLoop(), GameLoopListener {
     fun unjoin(session: Session) {
         session.userData = null
         add_post_runnable {
-            val player = game_map.remove(session.id)
-            player?.session = null
-        }
-    }
-
-    fun on_key_pressed(keyCode: Int, down: Boolean, session: Session) {
-        if (down) {
-            when (keyCode) {
-                87 -> add_post_runnable {
-                    (session.userData as? Player?)?.run {
-                        share(center.plus(screen_point))
-                    }
-                }
-                32 -> add_post_runnable {
-                    (session.userData as? Player?)?.split()
-                }
+            sessions += session
+            game_map.remove(session.id)?.let {
+                world?.remove(it.bubble)
             }
         }
     }
 
-    fun on_mouse_moved(x: Int, y: Int, session: Session) {
-        (session.userData as? Player?)?.screen_point?.set(x.toFloat(), y.toFloat())
+    fun rejoin(session: Session) {
+        unjoin(session)
+        join(session)
     }
 
-    fun set_protocol_data(protocolData: Protocol?) {
-        this.protocol_data = protocolData
+    fun on_mouse_moved(x: Int, y: Int, session: Session) {
+        (session.userData as? Player?)?.screen_point?.set(x.toFloat(), y.toFloat())
     }
 }
